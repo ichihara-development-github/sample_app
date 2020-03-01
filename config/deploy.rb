44c0a59cb433f7b0d904fffc0e281c10be45d300
ichihara-development-github <haken.ichihara@icloud.com>
2020-03-01 13:47:49 +0900

    update__rugy _version

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'github:ichihara-development-github/sample_app.git'
 #cloneを行うリモートリポジトリを記載
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.3'
+set :rbenv_ruby, '2.6.5'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
