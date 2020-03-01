5db094b1648b1bc6c2e9748ea479b9f8921ebf87
ichihara-development-github <haken.ichihara@icloud.com>
2020-03-01 17:39:48 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server '3.21.59.95', user: 'sample_user', roles: %w{app db web}
 
 
 set :ssh_options, {
-  keys: %w(~/.ssh/sample_app_key_rsa), #秘密キーのpathを記載
+  keys: %w(~/.ssh/sample_app.pem), #秘密キーのpathを記載
   forward_agent: true,
   auth_methods: %w(publickey),
   port: 22
